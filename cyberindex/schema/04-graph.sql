5d878f5ba7ef113a55cf5814b44afd7e485606e1
mrlp4 <laptanovi4@gmail.com>
2024-05-03 22:35:14 +0200

    setup debug, cyber:v4.0.0-rc1, index:v2.0.0-rc1

--- cyberindex/schema/04-graph.sql
+++ cyberindex/schema/04-graph.sql
@@ -6,8 +6,13 @@ CREATE TABLE cyberlinks
     neuron              TEXT                        NOT NULL REFERENCES account (address),
     timestamp           TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     height              BIGINT                      NOT NULL REFERENCES block (height),
-    transaction_hash    TEXT                        NOT NULL REFERENCES transaction (hash)
+    transaction_hash    TEXT                        NOT NULL
 );
+CREATE INDEX cyberlinks_particle_from_index ON cyberlinks(particle_from);
+CREATE INDEX cyberlinks_particle_to_index ON cyberlinks(particle_to);
+CREATE INDEX cyberlinks_neuron_index ON cyberlinks(neuron);
+CREATE INDEX cyberlinks_height_index ON cyberlinks(height);
+CREATE INDEX cyberlinks_from_to_index ON cyberlinks(particle_from, particle_to);
 
 CREATE TABLE particles
 (
@@ -16,5 +21,8 @@ CREATE TABLE particles
     neuron              TEXT                        NOT NULL REFERENCES account (address),
     timestamp           TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     height              BIGINT                      NOT NULL REFERENCES block (height),
-    transaction_hash    TEXT                        NOT NULL REFERENCES transaction (hash)
-)
+    transaction_hash    TEXT                        NOT NULL
+);
+CREATE INDEX particles_neuron_index ON particles(neuron);
+CREATE INDEX particles_height_index ON particles(height);
+CREATE INDEX particles_neuron_particle_index ON particles(neuron, particle);
